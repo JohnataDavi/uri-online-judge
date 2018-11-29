392bcedc1f0a23ccf4a06c0e566690f847b35cc5
Johnata <infojohnata@gmail.com>
2018-11-28 22:19:41 -0200

    v 2

--- 1436.py
+++ 1436.py
@@ -1,17 +1,7 @@
-lista, ldif = [], []
+lista = []
 n = int(input())
-for i in range(n):
-	i = int(input())
-	lista =  list (map (int, input().split()))
-	print(len(lista))
-	print(lista)
-	for i in range(i-1):
-		if i > 0:
-			if lista[i-1] > lista[i]:	
-				ldif.append(lista[i-1] - lista[i])
-			else:
-				ldif.append(lista[i] - lista[i-1])
-
-print(min(lista))
-print(min(ldif))
-print(ldif.index(min(ldif)))
+for j in range(n):
+    lista = list(map (int, input().split()))
+    lista.pop(0)
+    tam = len(lista) - 1
+    print("Case {}: {}".format(j+1, lista[int(tam/2)]))
