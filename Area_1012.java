4223d6ddb0c25bdc896c988355d98d8695bffeba
Johnata Davi <infojohnata@gmail.com>
2019-04-05 12:02:57 -0300

    1.0.0

---
+++ Area_1012.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package URI.Iniciante;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Johnata
+ */
+public class Area_1012 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double tri = (a * c) / 2;
+        double cir = 3.14159 * Math.pow(c, 2);
+        double tra = (a + b) * c / 2;
+        double qua = Math.pow(b, 2);
+        double ret = a * b;
+        System.out.printf("TRIANGULO: %.3f%n", tri);
+        System.out.printf("CIRCULO: %.3f%n", cir);
+        System.out.printf("TRAPEZIO: %.3f%n", tra);
+        System.out.printf("QUADRADO: %.3f%n", qua);
+        System.out.printf("RETANGULO: %.3f%n", ret);
+    }
+}
---
+++ Iccanobif_2807.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package URI.Iniciante;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Johnata
+ */
+public class Iccanobif_2807 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int prox = 1, ant = 0, res = 0;
+        int vet[] = new int[n];
+        vet[n - 1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            res = prox + ant;
+            vet[n - i - 1] = res;
+            ant = prox;
+            prox = res;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                System.out.print(vet[i]);
+            } else {
+                System.out.print(vet[i] + " ");
+            }
+        }
+        System.out.print("\n");
+    }
+}
