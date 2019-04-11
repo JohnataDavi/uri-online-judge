bbc923b9368fde6c2f38b60303686499e83e7431
Johnata Davi <infojohnata@gmail.com>
2019-04-10 23:09:19 -0300

    URI 1016.java

---
+++ Distancia_1016.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package URI.Iniciante;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Johnata
+ */
+public class Distancia_1016 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int x = sc.nextInt();
+         int result = x * 2;
+         System.out.println(result+" minutos");
+    }
+}
