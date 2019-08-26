a068b789fe072bd3e14aca4dee1ae96097684ab6
Johnata Davi <infojohnata@gmail.com>
2019-08-25 23:31:30 -0300

    URI 2516.cpp

---
+++ 2516.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+
+int main(){
+    long int n, r;
+    while(cin>>n){
+        if(n%2 ==0){
+            for(int i=0; i<n; i++){
+                r =
+            }
+        }else{
+            for(int i=0; i<n; i++){
+                r =
+            }
+        }
+    }
+    return 0;
+}
