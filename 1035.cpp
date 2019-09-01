1ef6af3d67a859ce494e09ae3007cf8c25f2bf22
Johnata Davi <infojohnata@gmail.com>
2019-09-01 10:15:47 -0300

    URI 1035.cpp

---
+++ 1035.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    int a, b, c, d, s;
+    cin >> a >> b >> c >> d;
+    cout << "\nDIFERENCA = " << ((a*b)-(c*d)) << "\n";
+
+    return (0);
+}
+
