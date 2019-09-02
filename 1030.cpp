34c893a7320ddaabaf2bdbbf065f16b8ef7c2b3f
Johnata Davi <infojohnata@gmail.com>
2019-09-02 20:16:23 -0300

    URI 1030.cpp

--- 1030.cpp
+++ 1030.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int f(int n, int k){
-	if(n>0){
+	if(n>0)
 		return (f(n - 1, k) + k - 1) % n + 1;
-	}else
+	else
 		return 0;
 }
 
@@ -14,7 +14,6 @@ int main(){
 	for(int i=1; i<=nc; i++){ 
 		cin >> n >> k;
 		cout << "Case " << i << ": "  << f(n, k) << endl;
-
 	}	
 	return 0;
 }
