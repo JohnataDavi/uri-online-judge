9c23cd1117b3adaf8d1f6762fbf6fc95aff467e2
Johnata Davi <infojohnata@gmail.com>
2021-03-15 13:44:13 -0300

    Produto Simples

---
+++ solutions/1004.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+ 
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int p = a*b;
+            System.out.println("PROD = "+p);
+    
+        }
+ 
+}
